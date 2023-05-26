c9439d41bb5f761eb300ec838ea08a785039093d
hubisan <hubisan@gmail.com>
2023-05-26 10:48:39 +0200

    Added additional functions skeletons.

--- woerterbuch.el
+++ woerterbuch.el
@@ -48,9 +48,13 @@
   "The function used to the display the org buffer with the synonyms."
   :type 'function)
 
-;;;; German Dictionary
+(defcustom woerterbuch-synonyms-list-bullet-point "-"
+  "String to use as list bullet point when converting the synonyms to a string."
+  :type 'string)
 
-;;;; German Thesaurus
+;;;; German Definitions
+
+;;;; German Synonyms
 
 (defvar woerterbuch--synonyms-openthesaurus-url
   (concat
@@ -58,17 +62,6 @@
    "&format=application/json"
    "&baseform=true")
   "Url to retrieve the synonyms for a word as JSON from openthesaurus.")
-
-(defun woerterbuch--synonyms-retrieve (word)
-  "Retrieve the synonyms for WORD as a list of lists.
-Each list consist of the synonyms for one meaning of the word.
-Returns nil if no synonyms are retrieved."
-  (let* ((raw-synonyms (woerterbuch--synonyms-retrieve-raw word))
-         (baseform (woerterbuch--synonyms-baseform raw-synonyms)))
-    ;; If a baseform was found use that to retrieve the synonyms.
-    (when baseform
-      (setq raw-synonyms (woerterbuch--synonyms-retrieve-raw baseform)))
-    (woerterbuch--synonyms-to-list raw-synonyms)))
 
 (defun woerterbuch--synonyms-retrieve-raw (word)
   "Return the synonyms for a WORD as plist as retrieved with the API."
@@ -97,10 +90,39 @@ Returns nil if not found."
   (map-elt raw-synonyms :baseforms)
   (car-safe (seq-into (plist-get raw-synonyms :baseforms) 'list)))
 
+(defun woerterbuch--synonyms-retrieve-as-list (word)
+  "Retrieve the synonyms for WORD as a list of lists.
+Each list consist of the synonyms for one meaning of the word.
+Returns nil if no synonyms are retrieved."
+  (let* ((raw-synonyms (woerterbuch--synonyms-retrieve-raw word))
+         (baseform (woerterbuch--synonyms-baseform raw-synonyms)))
+    ;; If a baseform was found use that to retrieve the synonyms.
+    (when baseform
+      (setq raw-synonyms (woerterbuch--synonyms-retrieve-raw baseform)))
+    (woerterbuch--synonyms-to-list raw-synonyms)))
+
+(defun woerterbuch--synonyms-convert-to-string (synonyms)
+  "Convert the list of SYNONYMS to a string.
+The string is a list. The group of synonyms for each meaning are
+shown as an item. The list bullet point can be configured with
+`woerterbuch-synonyms-list-bullet-point'"
+
+  )
+
+(defun woerterbuch--synonyms-retrieve-as-string (word)
+  "Retrieve the synonyms for WORD as a string.
+Returns nil if no synonyms are retrieved.
+The returned string groups the synonyms for each meaning on one line.
+It looks as follows:
+- Erprobung, Probe, Prüfung
+- Leistungsnachweis, Prüfung, Test
+- etc."
+  )
+
 ;;;###autoload
 (defun woerterbuch-synonyms-show-in-org-buffer (&optional word)
   "Show the synonyms for WORD in an `org-mode' buffer."
-  (interactive "P")
+  (interactive "sWort: ")
 
   )
 
@@ -110,25 +132,36 @@ Returns nil if not found."
 Will insert a list with each item being the synonyms for a meaning.
 If INCLUDE-HEADING is non-nil it inserts a subheading with the WORD as text and
 the list of synonyms below."
-  (interactive "P")
+  (interactive "sWort: \nP")
 
   )
 
 ;;;###autoload
 (defun woerterbuch-synonyms-kill-as-org-mode-syntax (&optional word include-heading)
   "Add the synonyms for WORD to the kill ring as `org-mode' syntax.
-Will add a list with each item being the synonyms for a meaning.
+Will add a list with each item being the synonyms for a meaning to the kill ring.
 If INCLUDE-HEADING is non-nil it will add a subheading with the WORD as text and
 the list of synonyms below."
-  (interactive "P")
+  (interactive "sWort: \nP")
 
   )
 
 ;;;###autoload
-(defun woerterbuch-synonyms-lookup (word &optional to-kill-ring)
+(defun woerterbuch-synonyms-lookup (&optional word to-kill-ring)
+  "Lookup synonyms for WORD and insert selected word at point.
+If TO-KILL-RING is non-nil it is added to the kill ring instead."
+  (interactive "sWort: \nP")
+
+  )
+
+;;;###autoloadpowerthesaurus-lookup-word-at-point
+(defun woerterbuch-synonyms-lookup-word-at-point (&optional to-kill-ring)
   "Lookup synonyms for WORD and insert selected word at point.
 If TO-KILL-RING is non-nil it is added to the kill ring instead."
   (interactive "P")
+  (let ((word (thing-at-point 'word)))
+
+    )
 
   )
 
