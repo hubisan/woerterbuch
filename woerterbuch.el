addd91d62bef304b4296dca50dc417b7fb8eb7cb
hubisan <hubisan@gmail.com>
2023-05-30 23:05:10 +0200

    All synonym parts seem to work. Have to write tests now.

--- woerterbuch.el
+++ woerterbuch.el
@@ -49,8 +49,9 @@
   :group 'convenience
   :link '(url-link "https://github.com/hubisan/woerterbuch"))
 
-(defcustom woerterbuch-synoyms-display-function nil
-  "The function used to the display the org buffer with the synonyms."
+(defcustom woerterbuch-org-buffer-display-function #'pop-to-buffer
+  "Function used to the display the org buffer with the definitions or synonyms.
+The function takes buffer as argument."
   :type 'function)
 
 (defcustom woerterbuch-synonyms-list-bullet-point "-"
@@ -61,6 +62,18 @@
   "Format used when inserting an Org heading before content."
   :type 'string)
 
+;;;; Major-Mode & Key Bindings
+
+(define-derived-mode woerterbuch-mode org-mode "Woerterbuch"
+  "Major mode for displaying woerterbuch buffer.")
+
+(define-key woerterbuch-mode-map (kbd "q") 'quit-window)
+
+;;;; Global Variables
+
+(defvar woerterbuch--org-buffer-name "*woerterbuch*"
+  "Name used for the Org buffer showing the defintions or synonyms.")
+
 ;;;; Auxiliary Functions
 
 (defun woerterbuch--org-add-heading (heading level content)
@@ -68,21 +81,34 @@
   (format woerterbuch-insert-org-heading-format
           (make-string level ?*) heading content))
 
-(defun woerterbuch--org-insert (text with-heading)
+(defun woerterbuch--org-insert (text with-heading &optional buffer)
   "Insert TEXT on current line if empty else on a new line.
 If WITH-HEADING is non-nil the text includes an Org heading and is inserted
 using `org-paste-subtree'. Will paste with the same level as the current
-heading."
-  (if with-heading
-      (if (eq major-mode 'org-mode)
-          (progn (require 'org)
-                 (org-paste-subtree (or (org-current-level) 1) text))
-        (user-error "Text can only be inserted in an Org buffer"))
-    (if (string-match-p "\\`\\s-*$" (thing-at-point 'line))
-        (kill-whole-line)
-      (end-of-line)
-      (newline))
-    (insert text)))
+heading. If BUFFER is given, insert the text into that buffer instead of the
+current."
+  (with-current-buffer (or buffer (current-buffer))
+    (if with-heading
+        (if (eq major-mode 'woerterbuch-mode)
+            (progn (require 'org)
+                   (org-paste-subtree (or (org-current-level) 1) text))
+          (user-error "Text can only be inserted in an Org buffer"))
+      (if (string-match-p "\\`\\s-*$" (thing-at-point 'line))
+          (kill-whole-line)
+        (end-of-line)
+        (newline))
+      (insert text))))
+
+(defun woerterbuch--get-word-at-point-or-selection ()
+  "Get the word at point or the selection if region is active.
+Returns a cons cell with the car being the word and cdr the bounds."
+  (if-let ((bounds (if (use-region-p)
+                       (cons (region-beginning) (region-end))
+                     (bounds-of-thing-at-point 'word))))
+      (cons
+       (buffer-substring-no-properties (car bounds) (cdr bounds))
+       bounds)
+    (user-error "no word at point and no region active")))
 
 ;;;; German Definitions
 
@@ -99,14 +125,16 @@ heading."
   "Return the synonyms for a WORD as plist as retrieved with the API."
   (let* ((url (format woerterbuch--synonyms-openthesaurus-url
                       (url-hexify-string word)))
-         (buffer (url-retrieve-synchronously url)))
+         (buffer (url-retrieve-synchronously url t)))
     (if (not buffer)
         (error "Could not retrieve synonyms")
       (with-current-buffer buffer
         (goto-char (point-min))
         (re-search-forward "^$")
         (forward-line)
-        (json-parse-buffer :object-type 'plist)))))
+        (prog1
+            (json-parse-buffer :object-type 'plist)
+          (kill-buffer buffer))))))
 
 (defun woerterbuch--synonyms-to-list (raw-synonyms)
   "Convert the RAW-SYNONYMS retrieved with the API to a list of lists.
@@ -126,7 +154,7 @@ Returns nil if not found."
   "Retrieve the synonyms for WORD as a list of lists.
 Each list consist of the synonyms for one meaning of the word.
 Returns a cons with car being the word and cdr the synonyms. The
-word is returned as it can differntiate from the WORD uses as
+word is returned as it can differntiate from the WORD used as
 parameter when a baseform is used to retrieve the synonyms.
 Returns nil if no synonyms are retrieved."
   (let* ((raw-synonyms (woerterbuch--synonyms-retrieve-raw word))
@@ -134,8 +162,9 @@ Returns nil if no synonyms are retrieved."
     ;; If a baseform was found use that to retrieve the synonyms.
     (when baseform
       (setq raw-synonyms (woerterbuch--synonyms-retrieve-raw baseform)))
-    (cons (or baseform word)
-          (woerterbuch--synonyms-to-list raw-synonyms))))
+    (let ((synonyms (woerterbuch--synonyms-to-list raw-synonyms)))
+      (when synonyms
+        (cons (or baseform word) synonyms)))))
 
 (defun woerterbuch--synonyms-convert-to-string (synonyms)
   "Convert the list of SYNONYMS to a string.
@@ -159,22 +188,46 @@ It looks as follows:
 - Erprobung, Probe, Prüfung
 - Leistungsnachweis, Prüfung, Test
 - etc."
-  (when-let ((synonyms-cons (woerterbuch--synonyms-retrieve-as-list word))
-             (word-used (car synonyms-cons))
-             (synonyms-list (cdr synonyms-cons))
+  (when-let ((word-and-synonyms (woerterbuch--synonyms-retrieve-as-list word))
+             (word-used (car word-and-synonyms))
+             (synonyms (cdr word-and-synonyms))
              (synonyms-string
               (format "%s\n"
-                      (woerterbuch--synonyms-convert-to-string synonyms-list))))
+                      (woerterbuch--synonyms-convert-to-string synonyms))))
     (if with-heading
         (woerterbuch--org-add-heading word-used 1 synonyms-string)
       synonyms-string)))
+
+(defun woerterbuch--read-synonym (word)
+  "Read a synonym for WORD in the minibuffer and return it.
+Returns nil if no synonym was selected.
+Signals an user-error if there are no synoyms for WORD."
+  (if-let ((word-and-synonyms (woerterbuch--synonyms-retrieve-as-list word))
+           (word-used (car-safe word-and-synonyms))
+           (synonyms (cdr-safe word-and-synonyms)))
+      (when-let ((synonyms-flattened (apply #'append synonyms))
+                 (synonyms-no-duplicates (seq-uniq synonyms-flattened))
+                 (synonyms-sorted (seq-sort #'string-lessp
+                                            synonyms-no-duplicates)))
+        (completing-read "Select synonym: " synonyms-sorted nil t))
+    (user-error "No synonyms found for %s" word)))
 
 ;;;###autoload
 (defun woerterbuch-synonyms-show-in-org-buffer (&optional word)
   "Show the synonyms for WORD in an `org-mode' buffer."
   (interactive "sWort: ")
-
-  )
+  (let ((buffer (get-buffer-create woerterbuch--org-buffer-name)))
+    (with-current-buffer buffer
+      (erase-buffer))
+    ;; FIXME Debug and maybe report bug (visual-fill-column-mode).
+    ;; Moved turning `org-mode' on after displaying the buffer. Else I had some
+    ;; problems with `visual-fill-column-mode' changing the margins of the
+    ;; current buffer window. This is most likely a bug in
+    ;; `visual-fill-column-mode'.
+    (funcall woerterbuch-org-buffer-display-function buffer)
+    (with-current-buffer buffer
+      (woerterbuch-mode)
+      (woerterbuch-synonyms-insert-into-org-buffer word t))))
 
 ;;;###autoload
 (defun woerterbuch-synonyms-insert-into-org-buffer (word &optional with-heading)
@@ -202,41 +255,31 @@ and the list of synonyms below."
   "Lookup synonyms for WORD and insert selected word at point.
 If TO-KILL-RING is non-nil it is added to the kill ring instead."
   (interactive "sWort: \nP")
-  ;; Remove duplicates and sort synonyms for word.
-  (completing-read
-    "prompt: "
-    '(("option1" 1) ("option" 2))
-    nil t))
+  (when-let ((synonym (woerterbuch--read-synonym word)))
+    (if to-kill-ring
+        (kill-new synonym)
+      (insert synonym))))
 
 ;;;###autoload
 (defun woerterbuch-synonyms-lookup-word-at-point ()
   "Lookup synonyms for word at point and add to kill ring."
   (interactive)
-  (let ((word (thing-at-point 'word)))
-
-    )
-
-  )
+  (if-let ((word-and-bounds (woerterbuch--get-word-at-point-or-selection))
+           (word (car word-and-bounds)))
+      (when-let ((synonym (woerterbuch--read-synonym word)))
+        (kill-new synonym))
+    (user-error "No word at point")))
 
 ;;;###autoload
 (defun woerterbuch-synonyms-replace-word-at-point ()
-  "Lookup synonyms for WORD at point and replace it with the selected synonym."
+  "Lookup synonyms for wort at point or selection and replace it."
   (interactive)
-  (if-let* ((bounds (if (use-region-p)
-                        (cons (region-beginning) (region-end))
-                      (bounds-of-thing-at-point 'word)))
-            (word (buffer-substring-no-properties (car bounds) (cdr bounds))))
-      (if-let ((word-and-synonyms (woerterbuch--synonyms-retrieve-as-list word))
-               (word (car word-and-synonyms))
-               (synonyms (cdr word-and-synonyms)))
-          (when-let ((synonyms-no-duplicates (seq-uniq synonyms))
-                     (synonyms-sorted (seq-sort #'string-lessp
-                                                synonyms-no-duplicates))
-                     (synonym (completing-read "Select synonym: "
-                                               synonyms-sorted nil t)))
-            (delete-region (car bounds) (cdr bounds))
-            (insert synonym))
-        (user-error "No synonyms found for %s" word))
+  (if-let ((word-and-bounds (woerterbuch--get-word-at-point-or-selection))
+           (word (car word-and-bounds))
+           (bounds (cdr word-and-bounds)))
+      (when-let ((synonym (woerterbuch--read-synonym word)))
+        (delete-region (car bounds) (cdr bounds))
+        (insert synonym))
     (user-error "No word at point")))
 
 (provide 'woerterbuch)
